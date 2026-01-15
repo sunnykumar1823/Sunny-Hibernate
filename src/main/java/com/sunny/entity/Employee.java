f4584b1d6ae94e39d1852b928f2e155752566781
sunnykumar2001 <sunnykumar18122001@gmail.com>
2026-01-15 22:17:18 +0530

    update

--- src/main/java/com/sunny/entity/Employee.java
+++ src/main/java/com/sunny/entity/Employee.java
@@ -1,9 +1,20 @@
 package com.sunny.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "empp")
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private String name, gender;
+	@Column(name = "e_Name")
+	private String name;
+	private String gender;
 	private int salary;
 
 	public Employee() {
