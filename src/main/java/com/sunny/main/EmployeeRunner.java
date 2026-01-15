d43ab0b79c91fe46e5cb475749d5edaa02dff74e
sunnykumar2001 <sunnykumar18122001@gmail.com>
2026-01-15 21:18:34 +0530

    update

--- src/main/java/com/sunny/main/EmployeeRunner.java
+++ src/main/java/com/sunny/main/EmployeeRunner.java
@@ -3,7 +3,8 @@ package com.sunny.main;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import com.sunny.entity.Employee;
 
@@ -11,10 +12,29 @@ public class EmployeeRunner {
 
 	public static void main(String[] args) {
 
-		Employee emp = new Employee("ram", "Male", 76000);
+		Employee emp = new Employee("sunny", "Male", 69000);
 
-		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		// first Approch
+//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+//		SessionFactory sessionFactory = cfg.buildSessionFactory();
+//		Session session = sessionFactory.openSession();
+//		Transaction tx = session.beginTransaction();
+
+		// Second Approch
+//		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+//		Session session = sessionFactory.openSession();
+//		Transaction tx = session.beginTransaction();
+
+		// Third Approch
+//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+//		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+//		SessionFactory sessionFactory = metadata.buildSessionFactory();
+//		Session session = sessionFactory.openSession();
+//		Transaction tx = session.beginTransaction();
+
+		SessionFactory sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder().configure().build())
+				.getMetadataBuilder().build().buildSessionFactory();
+
 		Session session = sessionFactory.openSession();
 		Transaction tx = session.beginTransaction();
 
