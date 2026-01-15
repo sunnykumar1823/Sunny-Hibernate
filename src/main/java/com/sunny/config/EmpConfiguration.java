eda8094bf6d0cd60f8b346d73fcdbb311c00e034
sunnykumar2001 <sunnykumar18122001@gmail.com>
2026-01-15 23:23:56 +0530

    update

---
+++ src/main/java/com/sunny/config/EmpConfiguration.java
@@ -0,0 +1,36 @@
+package com.sunny.config;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+public class EmpConfiguration {
+
+	public static SessionFactory getSessionFactory() {
+
+		Properties properties = new Properties();
+
+		properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+		properties.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://localhost:3306/hibernate");
+		properties.put(Environment.JAKARTA_JDBC_USER, "root");
+		properties.put(Environment.JAKARTA_JDBC_PASSWORD, "sunny@123");
+		properties.put(Environment.HBM2DDL_AUTO, "create");
+		properties.put(Environment.SHOW_SQL, "true");
+		properties.put(Environment.FORMAT_SQL, "true");
+
+//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(properties).build();
+//		Metadata metaData = new MetadataSources(ssr).addAnnotatedClass(com.sunny.entity.Employee.class)
+//				.getMetadataBuilder().build();
+//		SessionFactory sessionFactory = metaData.buildSessionFactory();
+//		
+//		return sessionFactory;
+
+		return new MetadataSources(new StandardServiceRegistryBuilder().applySettings(properties).build())
+				.addAnnotatedClass(com.sunny.entity.Employee.class).getMetadataBuilder().build().buildSessionFactory();
+
+	}
+
+}
--- src/main/java/com/sunny/main/EmployeeRunner.java
+++ src/main/java/com/sunny/main/EmployeeRunner.java
@@ -1,11 +1,9 @@
 package com.sunny.main;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import com.sunny.config.EmpConfiguration;
 import com.sunny.entity.Employee;
 
 public class EmployeeRunner {
@@ -14,28 +12,7 @@ public class EmployeeRunner {
 
 		Employee emp = new Employee("sunny", "Male", 69000);
 
-		// first Approch
-//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-//		SessionFactory sessionFactory = cfg.buildSessionFactory();
-//		Session session = sessionFactory.openSession();
-//		Transaction tx = session.beginTransaction();
-
-		// Second Approch
-//		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-//		Session session = sessionFactory.openSession();
-//		Transaction tx = session.beginTransaction();
-
-		// Third Approch
-//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
-//		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
-//		SessionFactory sessionFactory = metadata.buildSessionFactory();
-//		Session session = sessionFactory.openSession();
-//		Transaction tx = session.beginTransaction();
-
-		SessionFactory sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder().configure().build())
-				.getMetadataBuilder().build().buildSessionFactory();
-
-		Session session = sessionFactory.openSession();
+		Session session = EmpConfiguration.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
 
 		session.persist(emp);
